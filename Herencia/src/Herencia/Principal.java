a2a9b51d6d6da7eba07da6920f4771df9d56800d
Gerardo <gerardotrajano@gmail.com>
2024-10-23 20:37:02 +0200

    Mi primer commit

---
+++ Herencia/src/Herencia/Principal.java
@@ -0,0 +1,9 @@
+package Herencia;
+
+public class Principal {
+	public static void main(String[]args) {
+		Estudiante estudiante1 = new Estudiante("Gerardo ","Corona",24 ,2388  ,  7.87f);
+		
+		estudiante1.mostrarDatos();
+	}
+}
